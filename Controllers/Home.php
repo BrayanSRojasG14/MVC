56939603592406e4ef26fe10e31bac58da64b02f
Brayan <bsrg2001@hotmail.com>
2021-02-27 18:13:11 -0500

    Se modifico en controlador, se creadores modelos y se conectaron

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -1,13 +1,21 @@
 <?php
-    class Home{
-        public function __construct(){}
+    class Home extends Controllers{
+        public function __construct()
+        {
+            parent::__construct();
+        }
         public function home($parems)
         {
-            echo "Mensaje desde el controlador";
+            //echo "Mensaje desde el controlador";
         }
         public function datos($params)
         {
             echo "Dato recibido: ".$params;
+        }
+        public function carrito($params)
+        {
+            $carrito = $this->model->getCarrito($params);
+            echo $carrito;
         }
     }
 ?>

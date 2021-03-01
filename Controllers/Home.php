b08d5b6f9c2426048139e787ac4b98db4c751e6a
Brayan <bsrg2001@hotmail.com>
2021-02-28 20:02:24 -0500

    Se termino la creacion del Miniframe, se unieron vistas con libreria y controladores

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -6,16 +6,7 @@
         }
         public function home($parems)
         {
-            //echo "Mensaje desde el controlador";
-        }
-        public function datos($params)
-        {
-            echo "Dato recibido: ".$params;
-        }
-        public function carrito($params)
-        {
-            $carrito = $this->model->getCarrito($params);
-            echo $carrito;
+            $this->views->getView($this,"home");
         }
     }
 ?>
--- Models/homeModel.php
+++ Models/homeModel.php
@@ -3,11 +3,7 @@
     {
         public function __construct()
         {
-            //echo "Mensaje desde el modelo";
-        }
-        public function getCarrito($params)
-        {
-            return "Datos del carrito No. ".$params;
+            
         }
     }
 ?>

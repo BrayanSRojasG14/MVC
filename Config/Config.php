6492c87a0bf04eb04877e3c667246579800cc909
Brayan <bsrg2001@hotmail.com>
2021-03-02 19:48:51 -0500

    Se definio un controlador para las vistas y como recibir informacion de la DB 6

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,5 @@
 
    //define("BASE_URL","http://localhost/MVC/Practica/");
     const BASE_URL = "http://localhost/MVC/Practica/";
-    const LIBS = "Libraries/";
-    const VIEWS = "Views/";
 
 ?>

3686d60cb092e3785562a2f7e98695419c809baf
Brayan <bsrg2001@hotmail.com>
2021-03-04 21:46:38 -0500

    se creo la BD y la conexion a traves de php

---
+++ SQL/Conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+    class Conexion{
+        private $host = "localhost";
+        private $user = "root";
+        private $password = "";
+        private $db = "db-abel-os";
+        private $conect;
+
+        public function __construct(){
+            $connectionString = "mysql:hos=" .$this->host.";dbname=".$this->db.";charset=utf8";
+            try{
+                $this->conect = new PDO($connectionString,$this->user,$this->password);
+                $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (Exception $e) {
+                $this->conect = 'Error de conexion';
+                echo "ERROR: ". $e->getMessage();
+            }
+        }
+    }
+?>

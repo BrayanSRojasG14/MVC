880638cd49da4b49834b70a860fdc8c22f737e44
Brayan <bsrg2001@hotmail.com>
2021-03-11 23:13:45 -0500

    Se hizo conexion con DB, ademas de modelos y controladores con las funciones basicas de DB

--- Config/Config.php
+++ Config/Config.php
@@ -3,4 +3,10 @@
    //define("BASE_URL","http://localhost/MVC/Practica/");
     const BASE_URL = "http://localhost/MVC/Practica/";
 
+    const DB_HOST = "localhost";
+    const DB_NAME = "tienda";
+    const DB_USER = "root";
+    const DB_PASSWORD = "";
+    const DB_CHARSET = "charset=utf8";
+
 ?>
